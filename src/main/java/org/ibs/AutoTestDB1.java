fcaa6df08af73f4f87166b71bf2ccbff5aa585ba
Danil_Pak <dan.nid@mail.ru>
2024-12-04 16:27:49 +0500

    Initial commit

---
+++ src/main/java/org/ibs/AutoTestDB1.java
@@ -0,0 +1,78 @@
+package org.ibs;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static java.sql.DriverManager.getConnection;
+
+public class AutoTestDB1 {
+    @Owner("Danil Pak")
+    @Test
+    @DisplayName("Проверка добавления/удаления товара (БД)")
+
+    public void test() throws SQLException {
+        Connection connection = getConnection(
+                "jdbc:h2:tcp://localhost:9092/mem:testdb",
+                "user",
+                "pass");
+
+        Statement statement = connection.createStatement();
+
+/** Проверям список товаров в БД
+ */
+        System.out.println("Исходная БД:");
+
+        String all = "Select * FROM FOOD";
+        ResultSet rs = statement.executeQuery(all);
+        while (rs.next()) {
+            int FOOD_ID = rs.getInt("FOOD_ID");
+            String FOOD_NAME = rs.getString("FOOD_NAME");
+            String FOOD_TYPE = rs.getString("FOOD_TYPE");
+            int FOOD_EXOTIC = rs.getInt("FOOD_EXOTIC");
+            System.out.printf("%d, %s, %s, %d%n", FOOD_ID, FOOD_NAME, FOOD_TYPE, FOOD_EXOTIC);
+
+        }
+        System.out.println("============================");
+
+/** Добавляем товар в БД
+ */
+        System.out.println("БД после добавления товара:");
+
+        statement.executeUpdate("INSERT INTO FOOD " +
+                "VALUES ('5', 'Картофель', 'VEGETABLE', '0')");
+
+        String all1 = "Select * FROM FOOD";
+        ResultSet rs1 = statement.executeQuery(all1);
+        while (rs1.next()) {
+            int FOOD_ID1 = rs1.getInt("FOOD_ID");
+            String FOOD_NAME1 = rs1.getString("FOOD_NAME");
+            String FOOD_TYPE1 = rs1.getString("FOOD_TYPE");
+            int FOOD_EXOTIC1 = rs1.getInt("FOOD_EXOTIC");
+            System.out.printf("%d, %s, %s, %d%n", FOOD_ID1, FOOD_NAME1, FOOD_TYPE1, FOOD_EXOTIC1);
+        }
+        System.out.println("============================");
+
+/** Удаляем товар из БД
+ */
+        System.out.println("БД после удаления товара:");
+        statement.executeUpdate("DELETE FROM FOOD WHERE FOOD_NAME = 'Картофель'");
+
+        String all2 = "Select * FROM FOOD";
+        ResultSet rs2 = statement.executeQuery(all1);
+        while (rs2.next()) {
+            int FOOD_ID2 = rs2.getInt("FOOD_ID");
+            String FOOD_NAME2 = rs2.getString("FOOD_NAME");
+            String FOOD_TYPE2 = rs2.getString("FOOD_TYPE");
+            int FOOD_EXOTIC2 = rs2.getInt("FOOD_EXOTIC");
+            System.out.printf("%d, %s, %s, %d%n", FOOD_ID2, FOOD_NAME2, FOOD_TYPE2, FOOD_EXOTIC2);
+
+        }
+    }
+}
+
---
+++ src/main/java/org/ibs/AutoTestVegetable.java
@@ -0,0 +1,116 @@
+package org.ibs;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class AutoTestVegetable {
+    @Owner("Danil Pak")
+    @DisplayName("Проверка добавления товара типа 'Овощ' в список")
+    @Test
+    void test() {
+        System.setProperty("webdriver.chromedriver.driver,",
+                "\\src\\test\\resources\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://qualit.appline.ru/");
+
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        /** Нажимаем на вкладку "Песочница" и в выпадающем списке выбираем вкладку "Товары"
+         */
+        WebElement btnSandbox = driver.findElement(By.xpath(
+                "//a[@class='nav-link dropdown-toggle']"));
+        btnSandbox.click();
+
+        WebElement btnGoods = driver.findElement(By.xpath("//a[@href='/food']"));
+        btnGoods.click();
+
+        /** Нажать на кнопку "Добавить"
+         */
+        WebElement btnAdd = driver.findElement(By.xpath("//button[@data-toggle='modal']"));
+        btnAdd.click();
+
+        //WebElement titleAddingGoods = driver.findElement(By.xpath("//h5[@id='editModalLabel']"));
+        //Assertions.assertEquals("Добавление товара", titleAddingGoods.getText(), "Не открылось окно");
+
+        /** Ввести данные товара в соответствующие в поля
+         */
+        WebElement btnInputNameOfGoods = driver.findElement(By.xpath(
+                "//input[@id='name']"));
+        btnInputNameOfGoods.sendKeys("Момордика");
+
+        WebElement btnTypeOfGoods = driver.findElement(By.xpath(
+                "//select[@id='type']/..//option[@value='VEGETABLE']"));
+        btnTypeOfGoods.click();
+
+        WebElement btnCheckBoxExotic = driver.findElement(By.xpath(
+                "//input[@id='exotic']"));
+        btnCheckBoxExotic.click();
+        Assertions.assertTrue(btnCheckBoxExotic.isSelected());
+
+        /** Нажать на кнопку "Сохранить"
+         */
+        WebElement btnSave = driver.findElement(By.xpath("//button[@id='save']"));
+        btnSave.click();
+
+        WebElement titleRow5 = driver.findElement(By.xpath(
+                "//tr[5]/th[@scope='row']"));
+        Assertions.assertEquals("5", titleRow5.getText(), "Not add");
+
+        /** Нажать на кнопку "Добавить"
+         */
+        WebElement btnAdd1 = driver.findElement(By.xpath(
+                "//button[@data-toggle='modal']"));
+        btnAdd1.click();
+
+        /** Ввести данные товара (без чек-бокса) в соответствующие в поля
+         */
+        WebElement btnInputNameOfGoods1 = driver.findElement(By.xpath(
+                "//input[@id='name']"));
+        btnInputNameOfGoods1.sendKeys("Картофель");
+
+        WebElement btnTypeOfGoods1 = driver.findElement(By.xpath(
+                "//select[@id='type']/..//option[@value='FRUIT']"));
+        btnTypeOfGoods1.click();
+
+        /** Нажать на кнопку "Сохранить"
+         */
+        WebElement btnSave1 = driver.findElement(By.xpath("//button[@id='save']"));
+        btnSave1.click();
+
+        WebElement titleRow6 = driver.findElement(By.xpath("//tr[6]/th[@scope='row']"));
+        Assertions.assertEquals("6", titleRow6.getText(), "Not add");
+
+
+        /**Выбрать вкладку "Песочница" и в выпадающем списке нажать на вкладку "Сброс данных"
+         */
+        WebElement btnSandbox1 = driver.findElement(By.xpath(
+                "//a[@class='nav-link dropdown-toggle']"));
+        btnSandbox1.click();
+
+        WebElement btnClear = driver.findElement(By.xpath("//a[@id='reset']"));
+        btnClear.click();
+
+        driver.close();
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+}
