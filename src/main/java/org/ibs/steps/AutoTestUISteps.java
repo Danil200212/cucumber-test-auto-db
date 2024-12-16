0d4093a08ce3a0737d1517af6948169bf78e6a21
Danil_Pak <dan.nid@mail.ru>
2024-12-16 16:22:06 +0500

    .

--- src/main/java/org/ibs/steps/AutoTestUISteps.java
+++ src/main/java/org/ibs/steps/AutoTestUISteps.java
@@ -23,26 +23,44 @@ import java.util.concurrent.TimeUnit;
 
 public class AutoTestUISteps {
     private RemoteWebDriver driver;
+    private Properties props;
+
+
 
     @BeforeEach
-    public void testVegetableAdd() throws MalformedURLException {
-        // Настройка ChromeOptions для Selenoid
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*"); // Добавляем аргумент для разрешения удаленного доступа
+    void setUp() throws MalformedURLException {
+        // Загрузка настроек из файла свойств
+        props = new Properties();
+        // Пример: загрузка из файла или системных переменных
+        // props.load(new FileInputStream("config.properties"));
 
-        // Настройка DesiredCapabilities для Selenoid
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        // Определяем, где запускается тест (локально или удаленно)
+        boolean isRemote = Boolean.parseBoolean(System.getProperty("remote", "true"));
 
-        // Добавляем параметры для Selenoid
-        Map<String, Object> selenoidOptions = new HashMap<>();
-        selenoidOptions.put("enableVNC", true); // Включение VNC
-        selenoidOptions.put("enableVideo", false); // Отключение записи видео
-        capabilities.setCapability("selenoid:options", selenoidOptions);
+        if (isRemote) {
+            // Удаленный запуск через Selenoid
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            Map<String, Object> selenoidOptions = new HashMap<>();
+            selenoidOptions.put("browserName", "chrome"); // Указываем браузер
+            selenoidOptions.put("browserVersion", "109.0"); // Указываем версию браузера
+            selenoidOptions.put("enableVNC", true); // Включаем VNC
+            selenoidOptions.put("enableVideo", false); // Отключаем запись видео
+            capabilities.setCapability("selenoid:options", selenoidOptions);
 
-        // Подключение к Selenoid Hub
-        WebDriver driver = new RemoteWebDriver(new URL("http://jenkins.applineselenoid.fvds.ru:4444/wd/hub"), capabilities);
+            // Подключение к Selenoid
+            driver = new RemoteWebDriver(
+                    URI.create(props.getProperty("SELENOID_URL", "http://jenkins.applineselenoid.fvds.ru:4444/wd/hub")).toURL(),
+                    capabilities
+            );
+        } else {
+            // Локальный запуск
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
 
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
     @И("загрузка драйвера")
--- src/main/java/org/ibs/steps/JenkinsDriver.java
+++ src/main/java/org/ibs/steps/JenkinsDriver.java
@@ -18,44 +18,44 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class JenkinsDriver {
-    private WebDriver driver;
-    private Properties props;
-
-    @BeforeAll
-    void setUp() throws MalformedURLException {
-        // Загрузка настроек из файла свойств
-        props = new Properties();
-        // Пример: загрузка из файла или системных переменных
-        // props.load(new FileInputStream("config.properties"));
-
-        // Определяем, где запускается тест (локально или удаленно)
-        boolean isRemote = Boolean.parseBoolean(System.getProperty("remote", "true"));
-
-        if (isRemote) {
-            // Удаленный запуск через Selenoid
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            Map<String, Object> selenoidOptions = new HashMap<>();
-            selenoidOptions.put("browserName", "chrome"); // Указываем браузер
-            selenoidOptions.put("browserVersion", "109.0"); // Указываем версию браузера
-            selenoidOptions.put("enableVNC", true); // Включаем VNC
-            selenoidOptions.put("enableVideo", false); // Отключаем запись видео
-            capabilities.setCapability("selenoid:options", selenoidOptions);
-
-            // Подключение к Selenoid
-            driver = new RemoteWebDriver(
-                    URI.create(props.getProperty("SELENOID_URL", "http://jenkins.applineselenoid.fvds.ru:4444/wd/hub")).toURL(),
-                    capabilities
-            );
-        } else {
-            // Локальный запуск
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-            driver = new ChromeDriver();
-        }
-
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-    }
+//    private WebDriver driver;
+//    private Properties props;
+//
+//    @BeforeAll
+//    void setUp() throws MalformedURLException {
+//        // Загрузка настроек из файла свойств
+//        props = new Properties();
+//        // Пример: загрузка из файла или системных переменных
+//        // props.load(new FileInputStream("config.properties"));
+//
+//        // Определяем, где запускается тест (локально или удаленно)
+//        boolean isRemote = Boolean.parseBoolean(System.getProperty("remote", "true"));
+//
+//        if (isRemote) {
+//            // Удаленный запуск через Selenoid
+//            DesiredCapabilities capabilities = new DesiredCapabilities();
+//            Map<String, Object> selenoidOptions = new HashMap<>();
+//            selenoidOptions.put("browserName", "chrome"); // Указываем браузер
+//            selenoidOptions.put("browserVersion", "109.0"); // Указываем версию браузера
+//            selenoidOptions.put("enableVNC", true); // Включаем VNC
+//            selenoidOptions.put("enableVideo", false); // Отключаем запись видео
+//            capabilities.setCapability("selenoid:options", selenoidOptions);
+//
+//            // Подключение к Selenoid
+//            driver = new RemoteWebDriver(
+//                    URI.create(props.getProperty("SELENOID_URL", "http://jenkins.applineselenoid.fvds.ru:4444/wd/hub")).toURL(),
+//                    capabilities
+//            );
+//        } else {
+//            // Локальный запуск
+//            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//            driver = new ChromeDriver();
+//        }
+//
+//        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.manage().window().maximize();
+//    }
 //    private RemoteWebDriver driver;
 //@И("драйвер Jenkins")
 //
@@ -96,5 +96,25 @@ public class JenkinsDriver {
 //        throw new RuntimeException(e);
 //    }
 
+    //    @BeforeEach
+//    public void testVegetableAdd() throws MalformedURLException {
+//        // Настройка ChromeOptions для Selenoid
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--remote-allow-origins=*"); // Добавляем аргумент для разрешения удаленного доступа
+//
+//        // Настройка DesiredCapabilities для Selenoid
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+//
+//        // Добавляем параметры для Selenoid
+//        Map<String, Object> selenoidOptions = new HashMap<>();
+//        selenoidOptions.put("enableVNC", true); // Включение VNC
+//        selenoidOptions.put("enableVideo", false); // Отключение записи видео
+//        capabilities.setCapability("selenoid:options", selenoidOptions);
+//
+//        // Подключение к Selenoid Hub
+//        WebDriver driver = new RemoteWebDriver(new URL("http://jenkins.applineselenoid.fvds.ru:4444/wd/hub"), capabilities);
+//
+//    }
 
 }
